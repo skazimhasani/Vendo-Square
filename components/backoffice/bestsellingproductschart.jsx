64cf00f28d836cdbb5ee6ebde888128a485f8e17
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-20 14:03:15 +0500

    Colors Changed

--- components/backoffice/bestsellingproductschart.jsx
+++ components/backoffice/bestsellingproductschart.jsx
@@ -14,16 +14,16 @@ export default function BestSellingProductsChart() {
         label: '# of Votes',
         data: [50, 10, 20, 20],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
+          'rgba(0, 0, 255, 0.7)',
+          'rgba(255, 0, 221, 0.7)',
+          'rgba(2, 139, 71, 0.7)',
+          'rgba(0, 0, 0, 0.7)',
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
+          'rgba(0, 0, 255, 0.3)',
+          'rgba(255, 0, 221, 0.3)',
+          'rgba(2, 139, 71, 0.3)',
+          'rgba(0, 0, 0, 0.3)',
         ],
         borderWidth: 1,
       },
@@ -33,7 +33,9 @@ export default function BestSellingProductsChart() {
     <div className="bg-slate-600 p-8 rounded-lg">
       <h2 className='text-xl font-bold mb-4'>Best Selling Chart</h2>
       {/* Charts */}
+      <div className="p-4">
       <Pie data={data} />
+      </div>
     </div>
   )
 }
