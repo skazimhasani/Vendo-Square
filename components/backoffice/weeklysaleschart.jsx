cce78315b5bb02c9275648515f39cd7b2bfa9376
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-21 10:51:53 +0500

    Weekly Chart Start

--- components/backoffice/weeklysaleschart.jsx
+++ components/backoffice/weeklysaleschart.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function WeeklySalesChart() {
   return (
-    <div>WeeklySalesChart</div>
+    <div className="bg-slate-600 p-8 rounded-lg">
+      <h2 className='text-xl font-bold mb-4'>Weekly Sales Chart</h2>
+      <div className='p-4'>
+        {/* Tabs */}
+        {/* Content to display */}
+      </div>
+    </div>
   )
 }
