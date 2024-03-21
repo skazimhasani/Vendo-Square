95cfa2c5e6337436eab646d7297c3ab406bdd034
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-21 11:14:08 +0500

    Working Tabs

--- components/backoffice/weeklysaleschart.jsx
+++ components/backoffice/weeklysaleschart.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 export default function WeeklySalesChart() {
+  const tabs= [
+    {
+      title: "Sales",
+      type: "sales",
+    },
+    {
+      title: "Orders",
+      type: "orders",
+    }
+  ];
+  const [chartToDisplay, setChartToDisplay] = useState("sales");
   return (
     <div className="bg-slate-600 p-8 rounded-lg">
       <h2 className='text-xl font-bold mb-4'>Weekly Sales Chart</h2>
@@ -8,15 +20,16 @@ export default function WeeklySalesChart() {
         {/* Tabs */}
         
 
-<div className="text-sm font-medium text-center text-gray-200 border-b border-gray-100 dark:text-gray-400 dark:border-gray-700">
+<div className="text-sm font-medium text-center text-gray-200 border-b
+ border-gray-100 dark:text-gray-400 dark:border-gray-700">
     <ul className="flex flex-wrap -mb-px">
-        <li className="me-2">
-            <button className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-300 hover:border-gray-100 dark:hover:text-gray-100">Sales</button>
+      {tabs.map((tab,i)=>{
+        return(
+          <li className="me-2" key={i}>
+            <button onClick={()=>setChartToDisplay(tab.type)} className={chartToDisplay == tab.type ? "inline-block p-4 text-orange-600 border-b-2 border-orange-600 rounded-t-lg active dark:text-orange-500 dark:border-orange-500" : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-300 hover:border-gray-100 dark:hover:text-gray-100" }>{tab.title}</button>
         </li>
-        <li className="me-2">
-            <button className="inline-block p-4 text-orange-600 border-b-2 border-orange-600 rounded-t-lg active dark:text-orange-500 dark:border-orange-500" aria-current="page">Orders</button>
-        </li>
-       
+        )
+      })} 
     </ul>
 </div>
 
@@ -25,3 +38,4 @@ export default function WeeklySalesChart() {
     </div>
   )
 }
+
