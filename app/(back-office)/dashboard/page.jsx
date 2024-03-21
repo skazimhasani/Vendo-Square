e3bbea1c246d9d3ea64ce1971d762a9e79914977
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-21 12:07:18 +0500

    Recent Order Start

--- app/(back-office)/dashboard/page.jsx
+++ app/(back-office)/dashboard/page.jsx
@@ -3,7 +3,7 @@ import Heading from "@/components/backoffice/heading";
 import LargeCards from "@/components/backoffice/largecards";
 import SmallCards from "@/components/backoffice/smallcards";
 import DashboardCharts from "@/components/backoffice/dashboardcharts";
-
+import CustomDataTable from "@/components/backoffice/customdatatable";
 
 export default function DashboardPage() {
   return (
@@ -16,6 +16,7 @@ export default function DashboardPage() {
       {/* Charts */}
       <DashboardCharts/>
       {/* Recent Orders Table */}
+      <CustomDataTable />
     </div>
   )
 }
---
+++ components/backoffice/customdatatable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CustomDataTable() {
+  return (
+    <div className="bg-slate-600 p-8 rounded-lg">
+    <h2 className='text-xl font-bold mb-4'>Recent Orders</h2>
+    </div>
+  )
+}
