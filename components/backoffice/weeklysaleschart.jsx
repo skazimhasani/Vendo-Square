ad149d1babb43d25921072c041517cef79544464
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-21 11:02:36 +0500

    Tabs Design

--- components/backoffice/weeklysaleschart.jsx
+++ components/backoffice/weeklysaleschart.jsx
@@ -6,6 +6,20 @@ export default function WeeklySalesChart() {
       <h2 className='text-xl font-bold mb-4'>Weekly Sales Chart</h2>
       <div className='p-4'>
         {/* Tabs */}
+        
+
+<div className="text-sm font-medium text-center text-gray-200 border-b border-gray-100 dark:text-gray-400 dark:border-gray-700">
+    <ul className="flex flex-wrap -mb-px">
+        <li className="me-2">
+            <button className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-300 hover:border-gray-100 dark:hover:text-gray-100">Sales</button>
+        </li>
+        <li className="me-2">
+            <button className="inline-block p-4 text-orange-600 border-b-2 border-orange-600 rounded-t-lg active dark:text-orange-500 dark:border-orange-500" aria-current="page">Orders</button>
+        </li>
+       
+    </ul>
+</div>
+
         {/* Content to display */}
       </div>
     </div>
