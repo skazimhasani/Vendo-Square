f2853d76fa009c0e5dccb385afeaffa52f00cf27
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-21 11:42:55 +0500

    Line Chart Design

--- components/backoffice/weeklysaleschart.jsx
+++ components/backoffice/weeklysaleschart.jsx
@@ -1,7 +1,58 @@
 "use client";
 import React, { useState } from 'react'
-
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
 export default function WeeklySalesChart() {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+ const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
   const tabs= [
     {
       title: "Sales",
@@ -12,7 +63,7 @@ export default function WeeklySalesChart() {
       type: "orders",
     }
   ];
-  const [chartToDisplay, setChartToDisplay] = useState("sales");
+  const [chartToDisplay, setChartToDisplay] = useState(tabs[0].type);
   return (
     <div className="bg-slate-600 p-8 rounded-lg">
       <h2 className='text-xl font-bold mb-4'>Weekly Sales Chart</h2>
@@ -26,14 +77,20 @@ export default function WeeklySalesChart() {
       {tabs.map((tab,i)=>{
         return(
           <li className="me-2" key={i}>
-            <button onClick={()=>setChartToDisplay(tab.type)} className={chartToDisplay == tab.type ? "inline-block p-4 text-orange-600 border-b-2 border-orange-600 rounded-t-lg active dark:text-orange-500 dark:border-orange-500" : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-300 hover:border-gray-100 dark:hover:text-gray-100" }>{tab.title}</button>
+            <button onClick={()=>setChartToDisplay(tab.type)}
+             className={chartToDisplay == tab.type ? "inline-block p-4 text-orange-600 border-b-2 border-orange-600 rounded-t-lg active dark:text-orange-500 dark:border-orange-500" : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-300 hover:border-gray-100 dark:hover:text-gray-100" }>{tab.title}</button>
         </li>
         )
       })} 
     </ul>
 </div>
-
         {/* Content to display */}
+        {tabs.map((tab,i)=>{
+          if(chartToDisplay == tab.type) {
+            return <Line options={options} data={data} />
+          }
+          return null;
+        })}
       </div>
     </div>
   )
