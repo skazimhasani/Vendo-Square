f4bddaa0fdc51d203ab77be754a20d1227e9dd4c
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-25 12:36:21 +0500

    Sidebar Map

--- components/backoffice/sidebar.jsx
+++ components/backoffice/sidebar.jsx
@@ -1,25 +1,70 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import logo from "../../public/logo.svg";
 import {
   BookImage,
-  Cat,
-  Compass,
   ExternalLink,
-  Framer,
   LayoutGrid,
   Settings,
   Tractor,
   Truck,
   User,
-  UserSquare,
-  Users,
   Users2,
   Warehouse,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
+  const sidebarLinks = [
+    {
+      title: "Dashboard",
+      icon: LayoutGrid,
+      href: "/dashboard",
+    },
+    {
+      title: "Catalog",
+      icon: BookImage,
+      href: "/dashboard/catalog",
+    },
+    {
+      title: "Customers",
+      icon: Users2,
+      href: "/dashboard/customers",
+    },
+    {
+      title: "Markets",
+      icon: Warehouse,
+      href: "/dashboard/markets",
+    },
+    {
+      title: "Farmers",
+      icon: Tractor,
+      href: "/dashboard/farmers",
+    },
+    {
+      title: "Orders",
+      icon: Truck,
+      href: "/dashboard/orders",
+    },
+    {
+      title: "Our staff",
+      icon: User,
+      href: "/dashboard/staff",
+    },
+    {
+      title: "Settings",
+      icon: Settings,
+      href: "/dashboard/settings",
+    },
+    {
+      title: "Online Store",
+      icon: ExternalLink,
+      href: "/",
+    },
+  ];
   return (
     <div
       className="w-64 space-y-6 dark:bg-slate-500 bg-white h-screen shadow-md text-black dark:text-slate-50
@@ -29,69 +74,19 @@ export default function Sidebar() {
         <Image src={logo} alt="logo" className="w-20 h-20" />
       </Link>
       <div className="space-y-3 flex flex-col mt-14">
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <LayoutGrid />
-          <span>Dashboard</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <BookImage />
-          <span>Cataloue</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <Users2 />
-          <span>Customers</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <Warehouse />
-          <span>Markets</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <Tractor />
-          <span>Farmers</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <Truck />
-          <span>Orders</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <User />
-          <span>Our Staff</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <Settings />
-          <span>Settings</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
-        >
-          <ExternalLink />
-          <span>Online Store</span>
-        </Link>
+        {sidebarLinks.map((item, i) => {
+          const Icon = item.icon;
+          return (
+            <Link
+              key={i}
+              href={item.href}
+              className="flex items-center space-x-3 px-6 py-2 border-l-4 border-green-600"
+            >
+              <Icon />
+              <span>{item.title}</span>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
