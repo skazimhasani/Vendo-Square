d1365a0aec421b574dcfdfd7f0f6d419c7035cd6
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-27 13:30:58 +0500

    Toaster

--- app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function NewCategoryPage() {
   return (
     <div>
-      <div className="flex items-center justify-between py-6 px-12">
+      <div className="flex items-center justify-between py-6 px-12 bg-slate-300 rounded-lg text-black dark:text-white">
         <h2 className="text-xl font-semibold">New Category</h2>
         <button>
           <X />
--- context/providers.jsx
+++ context/providers.jsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-
+import { Toaster } from "react-hot-toast";
 export default function Providers({ children }) {
   return (
     <NextThemesProvider attribute="class" defaultTheme="dark">
+      <Toaster position="top-center" reverseOrder={false} />
       {children}
     </NextThemesProvider>
   );
