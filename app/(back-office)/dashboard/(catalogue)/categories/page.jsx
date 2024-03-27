a5f23a17c0e24b9cd43e60ddac385b05169036c7
Kazim Hasani <smkazimhasani@gmail.com>
2024-03-27 12:04:18 +0500

    Heading Page

--- app/(back-office)/dashboard/(catalogue)/categories/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/page.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import PageHeader from "../../../../../components/backoffice/pageheader";
 export default function CategoriesPage() {
   return (
     <div>
       {/*Header*/}
-      {/*Table*/}
+      <PageHeader
+        title="Categories"
+        href="/dashboard/categories/new"
+        linkTitle="Add New Category"
+      />
+      {/*Table Actions*/}
+      {/*Export || Search || Bulk Action*/}
     </div>
   );
 }
--- components/backoffice/pageheader.jsx
+++ components/backoffice/pageheader.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Heading from "@/components/backoffice/heading";
 import { Plus } from "lucide-react";
 import Link from "next/link";
-export default function PageHeader({}) {
+export default function PageHeader({ title, href, linkTitle }) {
   return (
     <div className="flex justify-between">
-      <Heading title="Categories" />
+      <Heading title={title} />
       <Link
-        href="/dashboard/categories/new"
+        href={href}
         className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 space-x-3"
       >
         <Plus />
-        <span>Add to Category</span>
+        <span>{linkTitle}</span>
       </Link>
     </div>
   );
